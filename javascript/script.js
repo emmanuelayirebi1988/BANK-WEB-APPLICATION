6d01905d6f71bacab12b7a86718299bcb9988be5
Emmanuel Ayirebi <emmanuel.ayirebi@amalitech.org>
2021-03-19 16:02:45 +0000

    added pages

---
+++ javascript/script.js
@@ -0,0 +1,54 @@
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+var depositBtn = document.querySelector("#depositBtn");
+var balance = document.querySelector("#balance");
+var depositAmnt = document.querySelector("#depositAmnt");
+var totalAmnt = 0.0;
+depositBtn.addEventListener('click', function(){
+  amount = parseFloat(depositAmnt.value);
+  totalAmnt = totalAmnt + amount;
+  balance.innerText = totalAmnt;
+})
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
